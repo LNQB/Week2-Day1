95302ec94416b9a59fce1fd84f5210f660c76fbd
LNQB-Dev <baodongthanh1994@gmail.com>
2025-09-15 14:03:30 +0700

    update

---
+++ prisma/migrations/20250915070013_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "public"."Book" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
+);
